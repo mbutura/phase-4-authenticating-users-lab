21bddaae2a1d7d7b3125d94b3e8753ea0180e650
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2023-04-01 18:44:40 +0000

    finalize lab

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,14 @@ class SessionsController < ApplicationController
         #Get username from JSON request
         user = User.find_by(username: params[:username])
         if user
-            session[:uid] = user.id
+            session[:user_id] = user.id
+            pp session
             render(json: user , session:  session)
         end
-        # else
-        #     render(json: {error: "Not authorized"} :unauthorized)
-        # end
+    end
+    def destroy
+        session.delete(:user_id)
+        head :no_content
     end
 
 end
